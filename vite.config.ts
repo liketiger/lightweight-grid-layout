88744b396292157573341f5cdc68811b984219d7
liketiger <edx2fire@likelion.org>
2025-07-22 22:05:08 +0900

    fix: modify entry point in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  // 1) demo용 index.html 무시
   publicDir: false,
+  plugins: [dts()],
 
-  // 2) 라이브러리 모드
   build: {
     target: 'es2020',
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'LightWeightGridLayout',
-      formats: ['es'], // 필요에 따라 'cjs','umd' 추가
+      formats: ['es'],
       fileName: (format) => `lightweight-grid-layout.${format}.js`,
     },
     rollupOptions: {
-      external: [], // peerDependencies 가 있으면 여기에
+      external: [],
       output: { globals: {} },
     },
   },
